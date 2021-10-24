39ecb7a4b252c420857184e9469bd0e9822e1f3c
Shaurya Vardhan <gsshaurya@gmail.com>
2021-10-24 16:48:09 +0530

    trying

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
-/** @type {import('@sveltejs/kit').Config} */
-// const netlify = import('@sveltejs/adapter-netlify');
+// const sveltePreprocess = require('svelte-preprocess');
+// const netlify = require('@sveltejs/adapter-netlify');
+// const pkg = require('./package.json');
+import sveltePreprocess from 'svelte-preprocess';
 import netlify from '@sveltejs/adapter-netlify';
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: sveltePreprocess(),
 	kit: {
+		// By default, `npm run build` will create a standard Node app.
+		// You can create optimized builds for different platforms by
+		// specifying a different adapter
+		adapter: netlify(),
+
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: netlify()
+		target: '#svelte'
 	}
 };
 
